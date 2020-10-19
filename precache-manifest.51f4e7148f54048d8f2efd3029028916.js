66fca8ad9c479e6f98eaa8a507a55ceeeab5c0be
Mustafa Hamraz <mustafa-hamraz@chasacademy.se>
2020-10-19 23:59:06 +0200

    Updates

---
+++ precache-manifest.51f4e7148f54048d8f2efd3029028916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250a8f5c9546c4da1fda4db82bda2cda",
+    "url": "/u09-react-weather-app/index.html"
+  },
+  {
+    "revision": "643098eb7ed8c57e4950",
+    "url": "/u09-react-weather-app/static/css/main.40a0baf3.chunk.css"
+  },
+  {
+    "revision": "299346c62a9cfd002e0e",
+    "url": "/u09-react-weather-app/static/js/2.3762774b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/u09-react-weather-app/static/js/2.3762774b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643098eb7ed8c57e4950",
+    "url": "/u09-react-weather-app/static/js/main.e9eca277.chunk.js"
+  },
+  {
+    "revision": "d3a9e832ec84da33eb89",
+    "url": "/u09-react-weather-app/static/js/runtime-main.a36c87ca.js"
+  }
+]);
